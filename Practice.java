5645cdd1fb48c519bcb1eff47a93aad72a90832a
Senanur <senanur.akbudak@thekaustschool.org>
2025-01-10 15:38:18 +0300

    7/01/2025
    Started coding my Idea which is a time zone converter for Turkiye KSA and USA

---
+++ Practice.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Practice {
+
+    public static void main(String[] args) {
+        System.out.println("We only Support KSA and US ");
+        Scanner user = new Scanner(System.in);
+        System.out.println("Your current time: ");
+        String currentTime = user.nextLine();
+
+        System.out.println("Your current location: ");
+        String currentLocation = user.nextLine();
+
+        System.out.println("Enter the Location you want to convert it to: ");
+        String convertedLocation = user.nextLine();
+
+
+        public static void times(Integer[]for (String arg : args) {
+            Integer ksaUS = Integer.valueOf(currentTime + 7);
+            integer USKsa = 7 - currentTime ;
+        }
+
+
+        }
+
+
+
+
+
+
+
+
+
+    }
+    }
+
